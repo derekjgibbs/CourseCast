5988857294e9c8b80bcc217334417b1b492cb150
Basti Ortiz <ortiz@bastidood.dev>
2025-07-28 13:46:08 +0800

    chore: add import sorter

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,18 @@
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 
+import imsort from "@bastidood/eslint-plugin-imsort";
 import { FlatCompat } from "@eslint/eslintrc";
 import { defineConfig } from "eslint/config";
 
 const compat = new FlatCompat({ baseDirectory: dirname(fileURLToPath(import.meta.url)) });
 export default defineConfig(
+  { ignores: ["src/convex/_generated/**/*"] },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     files: ["src/**/*.{j,t}s{x,}"],
+    extends: [imsort.configs.all],
+    plugins: { "@bastidood/imsort": imsort },
     rules: {
       "@typescript-eslint/no-unused-vars": [
         "error",
@@ -16,5 +20,4 @@ export default defineConfig(
       ],
     },
   },
-  { ignores: ["src/convex/_generated/**/*"] },
 );
