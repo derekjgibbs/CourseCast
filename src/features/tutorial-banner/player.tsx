79a91b7cd953f22d049c6670d321817bce47bf94
Basti Ortiz <ortiz@bastidood.dev>
2025-08-13 15:22:07 +0800

    chore: update Loom tutorial video

--- src/features/tutorial-banner/player.tsx
+++ src/features/tutorial-banner/player.tsx
@@ -48,7 +48,7 @@ export function TutorialPlayerContent() {
       <DialogHeader>
         <DialogTitle>How to use CourseCast</DialogTitle>
       </DialogHeader>
-      <LoomVideoPlayer href="https://www.loom.com/share/98d983826bd44913879d12614cfc6a3f" />
+      <LoomVideoPlayer href="https://www.loom.com/share/5b474f8dabc64b28a2c381b2803d7731" />
     </DialogContent>
   );
 }
